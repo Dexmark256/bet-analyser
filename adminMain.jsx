b854fd3288097aa1f35b7c0bb8362ef9814a3c11
Dexmark Tech <dexmarkdexmark@gmail.com>
2026-09-02 12:13:09 +0300

    Delete adminMain.jsx

--- adminMain.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import AdminApp from "./Admin.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <AdminApp />
-  </React.StrictMode>
-);
